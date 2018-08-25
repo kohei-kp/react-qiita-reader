d110f602ed5bfc52582a82965513cfa0f2070c56
kohei-kp <cowhey1991@gmail.com>
2018-08-25 21:25:27 +0900

    feat: 記事一覧の取得 + 記事内容の表示

--- src/components/QiitaReaderPage/index.js
+++ src/components/QiitaReaderPage/index.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react'
 import './index.css'
+import api from '../../common/api'
 
 class QiitaReaderPage extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      qiitaPostList: [{}],
+      selectedPostId: ''
+    }
+
+    api.getQiitaPost()
+      .then(res => {
+        this.setState({ qiitaPostList: res })
+      })
+      .catch(err => {
+        alert(err)
+      })
+  }
+
+  getSelectedContent () {
+    return this.state.qiitaPostList.find(post => post.id === this.state.selectedPostId) || {}
+  }
+
+  handleClickPost (id) {
+    this.setState({ selectedPostId: id })
+  }
+  
   render () {
+    const renderedBody = this.getSelectedContent().hasOwnProperty('rendered_body')
+      ? this.getSelectedContent().rendered_body
+      : ''
+    
     return (
       <div className="grid">
         <div style={{ gridColumn: '1 / 2', border: '1px solid #ccc' }}>
@@ -14,20 +44,17 @@ class QiitaReaderPage extends Component {
           <h1>記事一覧</h1>
           <div className="post-list">
             <ul>
-              <li>Sample Sample Sample1</li>
-              <li>Sample Sample Sample2</li>
-              <li>Sample Sample Sample3</li>
-              <li>Sample Sample Sample4</li>
-              <li>Sample Sample Sample5</li>
-              <li>Sample Sample Sample6</li>
+              {
+                this.state.qiitaPostList.map(post => {
+                  return <li onClick={ this.handleClickPost.bind(this, post.id) }>{ post.title }</li>
+                })
+              }
             </ul>
           </div>
         </div>
         <div style={{ 'gridColumn': '2 / 3', border: '1px solid #ccc' }}>
           <h1>記事内容</h1>
-          <div>
-            Content Content Content
-          </div>
+          <div dangerouslySetInnerHTML={{__html: renderedBody }}></div>
         </div>
       </div>
     )
